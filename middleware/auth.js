028544618febda04193b3fa900a1a9e4c4f87d6c
Brandon Ha <40842018+iDevBrandon@users.noreply.github.com>
2021-12-25 14:06:16 +0900

    fix error! - was getting token invalid

--- middleware/auth.js
+++ middleware/auth.js
@@ -10,13 +10,17 @@ module.exports = function (req, res, next) {
     return res.status(401).json({ msg: "No token, authorization denied" });
   }
 
-  // verify token
+  // Verify token
   try {
-    const decoded = jwt.verify(token, config.get("jwtSecret"));
-
-    req.user = decoded.user;
-    next();
+    jwt.verify(token, config.get("jwtSecret"), (error, decoded) => {
+      if (error) {
+        return res.status(401).json({ msg: "Token is not valid" });
+      } else {
+        req.user = decoded.user;
+        next();
+      }
+    });
   } catch (err) {
-    res.status(401).json({ msg: "Token is not valid" });
+    res.status(401).json({ msg: "Server Error" });
   }
 };
