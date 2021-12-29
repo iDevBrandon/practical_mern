b47bcedc2bbfd3612bc921e515b2edc691a7ddb5
Brandon Ha <40842018+iDevBrandon@users.noreply.github.com>
2021-12-29 12:52:13 +0900

    14. User auth - Login route

--- README.md
+++ README.md
@@ -16,6 +16,13 @@ Apply JWT to handle authentication with a unique token
 
 passport.js - we use passport to handle authentication with Google or Facebook
 
+Lecture 14 Login route
+
+in users.js, we copied pretty much everything from users.js
+
+editted to compare for matching email & password.
+If the email and password match, we will return a token
+
 ## Frontend sturucture
 
 React/Redux
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
 const auth = require("../../middleware/auth");
 const User = require("../../models/User");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const { body, validationResult } = require("express-validator");
 
 // @route   GET api/auth
 // @desc    Get test route
@@ -15,5 +19,63 @@ router.get("/", auth, async (req, res) => {
     res.status(500).send("Server Error");
   }
 });
+
+// @route   POST api/auth
+// @desc    Authenticate user & get token
+// @access  Public
+router.post(
+  "/",
+
+  body("email", "Email is required").isEmail(),
+  body("password", "Password is required").exists(),
+
+  async (req, res) => {
+    // console.log(req.body); access the data from req.body
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+    try {
+      // see if user exists
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid Credentials" }] });
+      }
+
+      // Compare to match password
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid Credentials" }] });
+      }
+
+      // return jsonwebtoken - to log in rightaway with the token
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: "360000" },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
 
 module.exports = router;
