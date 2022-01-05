80dce194a572aa043675c7ac7b02dbf192ecdbaa
Brandon Ha <idevbrandon@gmail.com>
2022-01-05 18:06:21 +0900

    35. Request example & login form

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Register = () => {
   const [formData, setFormData] = useState({
@@ -13,15 +14,14 @@ const Register = () => {
   const onChange = (e) =>
     setFormData({ ...formData, [e.target.name]: e.target.value });
 
-  const onSubmit = (e) => {
+  const onSubmit = async (e) => {
     e.preventDefault();
     if (password !== password2) {
       console.log("Passwords do not match");
     } else {
-      console.log("Go ahead");
+      console.log("SUCCESS");
     }
   };
-
   return (
     <>
       <h1 className="large text-primary">Sign Up</h1>
@@ -80,7 +80,7 @@ const Register = () => {
         <input type="submit" className="btn btn-primary" value="Register" />
       </form>
       <p className="my-1">
-        Already have an account? <a href="login.html">Sign In</a>
+        Already have an account? <Link to="/login">Login</Link>
       </p>
     </>
   );
