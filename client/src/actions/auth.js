02dbaa14d8ed857523fd07b11487c327b16f7188
Brandon Ha <idevbrandon@gmail.com>
2022-01-10 19:04:24 +0900

    46. Set if the user has a profile or not and then nevigate to profile page

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -9,6 +9,7 @@ import {
   LOGIN_SUCCESS,
   LOGIN_FAIL,
   LOGOUT,
+  CLEAR_PROFILE,
 } from "./types";
 
 // Load User - if token is in LS, then always send that token to the server
@@ -93,6 +94,9 @@ export const login = (email, password) => async (dispatch) => {
 
 // Logout / Clear profile
 export const logout = () => (dispatch) => {
+  dispatch({
+    type: CLEAR_PROFILE,
+  });
   dispatch({
     type: LOGOUT,
   });
