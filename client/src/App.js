3ce49247c2101b10ff94153bc5e17b7b94452235
Brandon Ha <idevbrandon@gmail.com>
2022-01-17 01:20:01 +0900

    5. Add education & experience

--- client/src/App.js
+++ client/src/App.js
@@ -17,6 +17,8 @@ import Dashboard from "./components/dashboard/Dashboard";
 import PrivateRoute from "./components/routing/PrivateRoute";
 import CreateProfile from "./components/profile-forms/CreateProfile";
 import EditProfile from "./components/profile-forms/EditProfile";
+import AddExperience from "./components/profile-forms/AddExperience";
+import AddEducation from "./components/profile-forms/AddEducation";
 
 if (localStorage.token) {
   setAuthToken(localStorage.token);
@@ -48,6 +50,14 @@ function App() {
             <Route
               path="edit-profile"
               element={<PrivateRoute component={EditProfile} />}
+            />
+            <Route
+              path="add-experience"
+              element={<PrivateRoute component={AddExperience} />}
+            />
+            <Route
+              path="add-education"
+              element={<PrivateRoute component={AddEducation} />}
             />
           </Routes>
         </section>
