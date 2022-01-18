62b6ccd0122890d630f2e6d2501cc816751069f4
Brandon Ha <40842018+iDevBrandon@users.noreply.github.com>
2022-01-19 03:04:50 +0900

    59. Displaying Github repos

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -8,6 +8,7 @@ import ProfileTop from "./ProfileTop";
 import ProfileAbout from "./ProfileAbout";
 import ProfileExperience from "./ProfileExperience";
 import ProfileEducation from "./ProfileEducation";
+import ProfileGithub from "./ProfileGithub";
 
 const Profile = ({ getProfileById, profile: { profile, loading }, auth }) => {
   const { id } = useParams();
@@ -17,8 +18,12 @@ const Profile = ({ getProfileById, profile: { profile, loading }, auth }) => {
   }, [id, getProfileById]);
   return (
     <>
+      {console.log(profile)}
       {profile === null || loading ? (
-        <Spinner />
+        <div>
+          No user found.
+          <Spinner />
+        </div>
       ) : (
         <>
           <Link to="/profiles" className="btn btn-light">
@@ -64,6 +69,10 @@ const Profile = ({ getProfileById, profile: { profile, loading }, auth }) => {
                 <h4>No education credentials</h4>
               )}
             </div>
+
+            {profile.githubusername && (
+              <ProfileGithub username={profile.githubusername} />
+            )}
           </div>
         </>
       )}
